fb65de393ad53ea0cb89be5a2dba25b7a28840da
Nico Breycha <nico.breycha@stud.h-da.de>
2024-01-03 01:57:06 +0100

    Added sorting function to find_files_by_type
    !R Added Sorting Option to find_files_by_type
    !F Added docstrings to all opt_helper_funcs functions

--- src/OpenPhotogrammetryToolkit/opt_helper_funcs.py
+++ src/OpenPhotogrammetryToolkit/opt_helper_funcs.py
@@ -5,6 +5,15 @@ import sys
 
 
 def get_class_name(module):
+    """
+    Retrieve the name of the class that has the same name as the module file.
+
+    :param module: The module from which to retrieve the class name
+    :type module: ModuleType
+    :return: The name of the class found in the module
+    :rtype: str
+    :raises AttributeError: If no class named as the module file is found in the module
+    """
     module_file = os.path.basename(module.__file__)
 
     file_name = os.path.splitext(module_file)[0]
@@ -17,6 +26,14 @@ def get_class_name(module):
 
 
 def import_module(file_location):
+    """
+    Import a module from a given file location.
+
+    :param file_location: The file location of the module to import
+    :type file_location: str
+    :return: The module that is imported
+    :rtype: ModuleType
+    """
     module_name = os.path.splitext(os.path.basename(file_location))[0]
 
     # Find the module specification from the import system
@@ -33,16 +50,18 @@ def import_module(file_location):
     return module
 
 
-def find_files_by_type(directory, file_type):
+def find_files_by_type(directory, file_type, sort=True):
     """
-    A robust function to find files of a specified type in a given directory.
+    Find files of a specified type in a given directory, optionally sorted by filename.
 
-    Args:
-    - directory (str): The path to the directory where the search should be performed.
-    - file_type (str): The extension of the files to find, e.g., 'txt' for text files.
-
-    Returns:
-    - list: A list of paths to the files found with the specified extension.
+    :param directory: The path to the directory where the search should be performed
+    :type directory: str
+    :param file_type: The extension of the files to find, e.g., 'txt' for text files
+    :type file_type: str
+    :param sort: Whether to return the files sorted by filename, defaults to True
+    :type sort: bool, optional
+    :return: A list of paths to the files found with the specified extension
+    :rtype: list
     """
     matched_files = []
     # Ensure the directory exists and is a directory, else return empty list
@@ -60,18 +79,23 @@ def find_files_by_type(directory, file_type):
             if file.endswith('.' + file_type):
                 matched_files.append(os.path.join(root, file))
 
+    if sort:
+        matched_files.sort(key=lambda x: os.path.basename(x))
+
     return matched_files
 
 
 def import_json_as_dict(json_file_path):
     """
-    Imports a JSON file as a dictionary.
+    Import a JSON file as a dictionary.
 
-    Args:
-    json_file_path (str): The file path of the JSON file.
-
-    Returns:
-    dict: The dictionary representation of the JSON file.
+    :param json_file_path: The file path of the JSON file
+    :type json_file_path: str
+    :return: The dictionary representation of the JSON file
+    :rtype: dict
+    :raises FileNotFoundError: If the JSON file is not found at the specified path
+    :raises JSONDecodeError: If the file is not a valid JSON
+    :raises Exception: For other exceptions that may occur
     """
     try:
         with open(json_file_path, 'r') as file:
